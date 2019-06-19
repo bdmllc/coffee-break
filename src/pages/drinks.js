447421cda00a0edc213b5aeef171182723868aa2
bdmllc <beachdaymedia@gmail.com>
2019-06-19 15:02:04 -0400

    added Title to drinks.js

--- src/pages/drinks.js
+++ src/pages/drinks.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-
+import Title from "../components/Globals/Title"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
@@ -15,7 +15,24 @@ const IndexPage = ({ data }) => (
       title="Drinks"
       styleClass="drinks"
     />
-    <Info />
+    <section className="py-5">
+      <div className="container">
+        <Title title="drinks" />
+        <div className="row">
+          <div className="col-10 col-sm-8 mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              Check out our list of affordable drinking establishments and Happy
+              Hours in Miami.
+            </p>
+            <Link to="/contact/">
+              <button className="btn btn-contact text-uppercase">
+                contact us
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 )
 
