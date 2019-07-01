b800310ace327cfb83b7bcaa8ba4834a3529ce06
bdmllc <beachdaymedia@gmail.com>
2019-07-01 19:02:39 -0400

    here we go fingers crossed 3

--- src/pages/drinks.js
+++ src/pages/drinks.js
@@ -58,7 +58,7 @@ export const query = graphql`
           price
           category
           image {
-            fixed(width: 250) {
+            fixed(width: 300) {
               ...GatsbyContentfulFixed_tracedSVG
             }
           }
