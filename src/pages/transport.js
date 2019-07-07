691e22a634465e8f28ef23af6008490ba82ccc46
bdmllc <beachdaymedia@gmail.com>
2019-07-07 18:56:58 -0400

    changed transport.js info

--- src/pages/transport.js
+++ src/pages/transport.js
@@ -19,8 +19,22 @@ const IndexPage = ({ data }) => (
     <section className="py-5">
       <div className="container">
         <Title title="transport" />
+        <br />
         <div className="row">
           <div className="col-10 col-sm-8 mx-auto text-center">
+            <h1>Rule # 1:</h1>
+            <br />
+            <h2>
+              DO NOT rely on the Miami Metrorail System to function properly on
+              Saturdays, Sundays and Holidays.
+            </h2>
+            <br />
+            <h3>
+              The value of Bed and Breakfast vacation rentals that rely on cheap
+              Metrorail transporation will be worth less on weekends and
+              holidays.
+            </h3>
+            <br />
             <p className="lead text-muted mb-5">
               Tour-O-Miami is your best source for tips on how to use Miami's
               public transportation service to get to where you want, when you
